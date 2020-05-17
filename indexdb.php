30318b89fef94a8ba927c54da18f7860d4bba309
Rebecca Li <Rebecca.CYLi@gmail.com>
2020-05-17 19:13:15 -0400

    new php stuff

--- indexdb.php
+++ indexdb.php
@@ -18,8 +18,10 @@ function pg_connection_string_from_database_url() {
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
 
 # Now let's use the connection for something silly just to prove it works:
-$result = pg_query($pg_conn, 'SELECT * FROM hattrick.appuser');
+$query = "SELECT * FROM hattrick.appuser"; 
+$result = pg_query($pg_conn, $query);
 $arr = pg_fetch_all($result);
+print_r($arr);
 
 print "<pre>\n";
 if (!pg_num_rows($result)) {
@@ -30,7 +32,7 @@ if (!pg_num_rows($result)) {
   while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
 }
 
-print_r($arr);
+
 
 
 
