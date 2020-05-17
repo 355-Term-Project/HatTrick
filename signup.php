751e551996706ed14b9893dca6d90f2fbd06c583
Rebecca Li <Rebecca.CYLi@gmail.com>
2020-05-17 16:02:38 -0400

    html-php setup

---
+++ signup.php
@@ -0,0 +1,12 @@
+<?php
+if ( isset($_POST['submit']) ){
+    $username = $_POST['username'];
+    $password =  $_POST['password'];
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $email = $_POST['email'];
+    $usertype = $_POST['usertype'];
+}
+
+
+?>
