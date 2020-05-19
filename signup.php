ad531944a98e56e4ae463fbeb1bac87fd1518659
zafar12196 <64497867+zafar12196@users.noreply.github.com>
2020-05-18 20:31:28 -0400

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,10 +1,20 @@
-<?php
-if ( isset($_POST['submit']) ){
-    $username = $_POST['username'];
-    $password =  $_POST['password'];
-    $firstName = $_POST['firstname'];
-    $lastName = $_POST['lastname'];
-    $email = $_POST['email'];
-    $usertype = $_POST['usertype'];
-}
+ <?php
+
+include_once 'connection.php'; 
+
+    
+    $id= $_POST['id'];
+    $SUBJECT= $_POST['SUBJECT'];
+    $content= $_POST['content'];
+    $DATE= $_POST['DATE'];
+
+
+
+    $sql = "INSERT INTO post (id, SUBJECT, content, DATE)
+    VALUES('$id', '$SUBJECT', '$content', '$DATE')";
+     mysqli_query($conn,$sql);
+
+    header("Location: Mainpage.php?signup=success");
+    
+
 ?>
